325ec0f91230682faeb3bf8139a3a4ff434d8ab0
Artemova Dina <artemova@perx.ru>
2025-11-30 12:53:39 +0300

    add habit model

--- users/models.py
+++ users/models.py
@@ -34,3 +34,33 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = "пользователь"
         verbose_name_plural = "пользователи"
+
+
+class Habit(models.Model):
+    """Модель привычки"""
+
+    owner = models.ForeignKey(
+        CustomUser,
+        verbose_name="Пользователь",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="habits",
+    )
+    place = models.CharField(max_length=250, verbose_name="Место выполнения привычки")
+    time = models.DateTimeField(verbose_name="Время, когда необходимо выполнять привычку", blank=True, null=True)
+    action = models.CharField(max_length=500, verbose_name="Действие, которое представляет собой привычка")
+    is_enjoyable = models.BooleanField(verbose_name="Привычка приятная?", blank=True, null=True)
+    related_habit = models.ForeignKey(
+        "self",
+        verbose_name="Связанная привычка",
+        blank=True,
+        null=True,
+        help_text="Укажите приятную привычку, которая будет вознаграждением",
+        on_delete=models.SET_NULL,
+        related_name="related_habits",
+    )
+    period_in_days = models.PositiveSmallIntegerField(verbose_name="Периодичность привычки в днях", default=1)
+    reward = models.CharField(max_length=500, verbose_name="Действие, которое является вознаграждением")
+    duration = models.PositiveSmallIntegerField(verbose_name="Продолжительность выполнения привычки в секундах")
+    is_public = models.BooleanField(verbose_name="Привычка публичная?", blank=True, null=True)
